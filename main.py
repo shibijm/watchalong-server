24d84b851de57c5765306e91db49e0c04449eb00
Shibi J M <6216943+shibijm@users.noreply.github.com>
2021-11-10 11:48:25 +0530

    Fixed error

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from modules import KeepAlive, WebSocketServer
 import os
 
 isHeroku = "PORT" in os.environ
-port = int(os.environ["port"]) if isHeroku else 22334
+port = int(os.environ["PORT"]) if isHeroku else 22334
 
 if isHeroku:
 	logger.info("Heroku detected, activating keep-alive mode")
