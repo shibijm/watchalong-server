afb6a3f6586243d9fc543b355033eee3d5c3548e
Shibi J M <6216943+shibijm@users.noreply.github.com>
2021-11-12 18:10:36 +0530

    Better protocol

--- models/User.py
+++ models/User.py
@@ -9,16 +9,17 @@ class User:
 	pingInterval = 30
 	pingTimeoutDelay = 15
 
-	def __init__(self, websocket: WebSocketServerProtocol, address: str, name: str) -> None:
+	def __init__(self, websocket: WebSocketServerProtocol, address: str, name: str, room: str) -> None:
 		self.websocket = websocket
 		self.address = address
 		self.name = name
+		self.room = room
 		self.disconnectReason = "Connection Closed"
 		self.pingTimer = AsyncTimer(1, self.ping)
 		self.pingTimeoutTimer: Optional[AsyncTimer] = None
 
 	async def ping(self) -> None:
-		await self.send({ "event": "PING" })
+		await self.send({ "event": "PING", "payload": None })
 		self.pingTimeoutTimer = AsyncTimer(self.pingTimeoutDelay, self.pingTimeout)
 
 	async def pingTimeout(self) -> None:
@@ -32,7 +33,7 @@ class User:
 
 	async def send(self, data: dict[str, Any]) -> None:
 		dataEncoded = json.dumps(data, separators = (",", ":"))
-		logger.info("[OUT] [%s - %s] %s", self.name, self.address, dataEncoded)
+		logger.info("[OUT] [%s] [%s - %s] %s", self.room, self.name, self.address, dataEncoded)
 		await self.websocket.send(dataEncoded)
 
 	async def sendMessage(self, message: str) -> None:
--- modules/WebSocketServer.py
+++ modules/WebSocketServer.py
@@ -22,64 +22,80 @@ class WebSocketServer():
 		server = await serve(self.connectionHandler, None, self.port, ping_interval = None, ping_timeout = None)
 		await server.wait_closed()
 
-	def broadcast(self, data: dict[str, Any], excludedUser: Optional[User] = None) -> None:
+	def broadcast(self, room: str, data: dict[str, Any], excludedUser: Optional[User] = None) -> None:
 		dataEncoded = json.dumps(data, separators = (",", ":"))
-		logger.info("[BROADCAST] %s", dataEncoded)
-		broadcast([user.websocket for user in users if user != excludedUser], dataEncoded)
+		logger.info("[BROADCAST] [%s] %s", room, dataEncoded)
+		broadcast([user.websocket for user in users if user != excludedUser and (user.room == room or not room)], dataEncoded)
 
-	def broadcastMessage(self, message: str, excludedUser: Optional[User] = None) -> None:
-		self.broadcast({ "event": "MESSAGE", "payload": message }, excludedUser)
+	def broadcastMessage(self, room: str, message: str, excludedUser: Optional[User] = None) -> None:
+		self.broadcast(room, { "event": "MESSAGE", "payload": message }, excludedUser)
 
 	async def connectionHandler(self, websocket: WebSocketServerProtocol, _path: str) -> None:
 		if "X-Forwarded-For" in websocket.request_headers:
 			address = websocket.request_headers["X-Forwarded-For"]
 		else:
 			address = f"{websocket.remote_address[0]}:{websocket.remote_address[1]}"
-		logger.info("[CONNECT] %s", address)
+		logger.info("[CONNECT] [%s]", address)
 		user: Optional[User] = None
 		try:
 			async for dataEncoded in websocket:
 				if not isinstance(dataEncoded, str):
+					logger.error("Invalid data received from %s", address)
 					continue
 				try:
 					data: dict[str, Any] = json.loads(dataEncoded)
 				except:
 					logger.error("Malformed JSON received from %s", address)
 					continue
-				if not user:
-					if data["event"] == "HANDSHAKE":
-						user = User(websocket, address, data["payload"]["name"])
-						users.append(user)
-						logger.info("[HANDSHAKE] %s - %s", user.name, user.address)
-						await user.sendMessage(f"Welcome, {user.name}. Users connected: {', '.join(user.name for user in users)}")
-						self.broadcastMessage(f"{user.name} has joined", user)
+				if "event" not in data or "payload" not in data:
+					logger.error("Incomplete data received from %s", address)
 					continue
-				logger.info("[IN] [%s - %s] %s", user.name, user.address, dataEncoded)
-				match data["event"]:
+				event = data["event"]
+				payload = data["payload"]
+				if not user:
+					logger.info("[IN] [%s] %s", address, dataEncoded)
+					if event == "HANDSHAKE":
+						if "name" not in payload or "room" not in payload:
+							logger.error("Incomplete data received from %s", address)
+							continue
+						name = payload["name"].strip()
+						room = payload["room"].strip()
+						if not name or not room:
+							logger.error("Incomplete data received from %s", address)
+							continue
+						user = User(websocket, address, name, room)
+						users.append(user)
+						self.broadcast(user.room, { "event": "USER_JOINED", "payload": { "name": user.name } }, user)
+						await user.send({ "event": "HANDSHAKE", "payload": None })
+					continue
+				logger.info("[IN] [%s] [%s - %s] %s", user.room, user.name, user.address, dataEncoded)
+				match event:
 					case "PONG":
 						user.handlePong()
 					case "USERS":
-						await user.sendMessage(f"Users connected: {', '.join(user.name for user in users)}")
-					case "PLAY_PAUSE_REQUEST":
-						action = data["payload"]["action"]
-						self.lowestTime = data["payload"]["time"]
+						await user.send({ "event": "USERS", "payload": [{ "name": user.name } for user in users]})
+					case "CONTROL_MEDIA":
+						if self.pendingAction:
+							continue # TODO: Send error message
+						action = payload["action"]
+						self.lowestTime = payload["time"]
 						totalOtherUsers = len(users) - 1
 						if totalOtherUsers == 0:
-							self.broadcast({ "event": "PLAY_PAUSE", "payload": { "action": action, "at": self.lowestTime }})
+							self.broadcast(user.room, { "event": "CONTROL_MEDIA", "payload": { "action": action, "at": self.lowestTime }})
 						else:
-							self.broadcast({ "event": "STATUS_REQUEST", "payload": { "action": action, "requestingUser": { "name": user.name } }}, user)
-							self.pendingResponses += totalOtherUsers
+							self.broadcast(user.room, { "event": "MEDIA_STATUS", "payload": { "action": action, "requestingUser": { "name": user.name } }}, user)
+							self.pendingResponses += totalOtherUsers # TODO: Track user-wise
 							self.pendingAction = action
-					case "STATUS_UPDATE":
-						if "error" in data["payload"]:
-							self.pendingResponses = 0
-							self.pendingAction = ""
-							self.broadcastMessage(f"{user.name} failed to connect to their media player. Reason: {data['payload']['error']['message']}", user)
-							continue
-						self.lowestTime = min(self.lowestTime, data["payload"]["time"])
+					case "MEDIA_STATUS":
+						if not self.pendingAction:
+							continue # TODO: Send error message
 						self.pendingResponses -= 1
+						if "error" in payload:
+							self.broadcastMessage(user.room, f"{user.name} failed to connect to their media player. Reason: {data['payload']['error']['message']}", user)
+							continue
+						self.lowestTime = min(self.lowestTime, payload["time"])
 						if self.pendingResponses == 0:
-							self.broadcast({ "event": "PLAY_PAUSE", "payload": { "action": self.pendingAction, "at": self.lowestTime }})
+							self.broadcast(user.room, { "event": "CONTROL_MEDIA", "payload": { "action": self.pendingAction, "at": self.lowestTime }})
 							self.pendingAction = ""
 					case "QUIT":
 						user.disconnectReason = "Quit"
@@ -89,8 +105,8 @@ class WebSocketServer():
 		except:
 			traceback.print_exc()
 		if user:
-			logger.info("[DISCONNECT] %s - %s (%s)", user.name, user.address, user.disconnectReason)
 			users.remove(user)
-			self.broadcastMessage(f"{user.name} has left ({user.disconnectReason})")
+			logger.info("[DISCONNECT] [%s] [%s - %s] %s", user.room, user.name, user.address, user.disconnectReason)
+			self.broadcast(user.room, { "event": "USER_LEFT", "payload": { "name": user.name, "reason": user.disconnectReason } })
 		else:
 			logger.info("[DISCONNECT] %s", address)
